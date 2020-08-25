2117ff609345e564f95ed351d3a00608a32f267d
tMalone <fyznitch@gmail.com>
2020-08-24 21:19:28 -0700

    git stt

---
+++ hello_world.js
@@ -0,0 +1,18 @@
+const array1 = [142,452,3,4, true, "cat, 'dog"]
+
+for(i of array1){
+    console.log(i);
+}
+
+function MakeCar(make, model, year) {
+    this.year = year;
+    this.make = make;
+    this.year = year
+    this. color = 'blue';
+
+}
+
+let nova = new MakeCar('chevy','nova',1979);
+
+console.log(nova);
+
