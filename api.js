ab82ca38ff0510beec6e7a6563b3f8bdd5e12b85
tMalone <fyznitch@gmail.com>
2020-08-26 22:28:15 -0700

    1st api

---
+++ api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const api = express();
+
+api.get('/', (req,res) => {
+    res.send("Success breeds success!!!!!");
+})
+
+api.listen(9090, () => console.log('Listning on port 9090'));
